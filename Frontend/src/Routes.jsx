8333be7e1b54ca800ad8d805acf0642c9d87e247
arpooja-githhub <pooja.a.r.2005@gmail.com>
2025-12-29 00:31:37 +0530

    Fixed routes

--- Frontend/src/Routes.jsx
+++ Frontend/src/Routes.jsx
@@ -2,13 +2,20 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import GoogleLogin from "./GoogleLogin.jsx";
 import NotFound from "./NotFound.jsx";
 import Layout from "./Layout.jsx";
-import { useContext } from "react";
+import { useContext,useEffect  } from "react";
 import { MyContext } from "./MyContext.jsx";
 import OAuthSuccess from "./0AuthSuccess.jsx";
 
 function AppRoutes() {
-  const { isAuthenticated } = useContext(MyContext);
+ 
+   const { isAuthenticated, setIsAuthenticated } = useContext(MyContext);
 
+  
+    useEffect(() => {
+      if (localStorage.getItem("token")) {
+        setIsAuthenticated(true);
+      }
+    }, [setIsAuthenticated]);
   return (
     <Routes>
       <Route path="/login" element={<GoogleLogin />} />
